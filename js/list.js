3d2b7d39043ac77f08edf6ff08b1461d573694b6
soumya3969 <srmhnt2001@gmail.com>
2023-10-04 12:26:15 +0530

    list added

---
+++ js/list.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.menu-btn');
+let nav = document.querySelector('nav')
+
+menuBtn.addEventListener('click',function(){
+    nav.classList.toggle('show-menu')
+})
